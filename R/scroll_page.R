5a581ba1adf263cda932b841f8d45aeec7c01d83
moamiristat <moamiristat@protonmail.com>
2022-03-07 17:47:16 -0500

    Implement 4 webpage scrolling funcs

---
+++ R/scroll_page.R
@@ -0,0 +1,112 @@
+#' Scroll to dynamically load more of the webpage
+#'
+#' @param remDr Remote client driver
+#'
+#' @return A character string
+#' @export
+#'
+#' @examples
+#' remDr <- RSelenium::rsDriver(port = 4446L,
+#' browser = "firefox", verbose = FALSE)$client
+#' url <- "https://www.ocado.com/browse"
+#' remDr$navigate(url)
+#' scroll_down_page_perc(remDr = remDr, perc = seq(0, 1, .25))
+#'
+#' # Close the server
+#' remDr$close()
+#' gc(remDr)
+#' rm(remDr)
+scroll_down_and_load <- function(remDr = remDr){
+
+  last_height <-
+    remDr$executeScript("return document.body.scrollHeight") %>%
+    unlist()
+
+  new_height <- 0
+
+  while(TRUE){
+    remDr$executeScript(
+      script = "window.scrollTo(0, document.body.scrollHeight)")
+
+    nytnyt(c(2, 3),
+           crayon_col = crayon::green,
+           "Scrolling to the bottom of the page\n")
+
+    new_height <-
+      remDr$executeScript("return document.body.scrollHeight") %>%
+      unlist()
+
+    print(list(old = last_height, new = new_height))
+
+    if(new_height == last_height){
+      break
+    }
+
+    last_height <- new_height
+  }
+  grocerycart::nytnyt(period = c(2, 5))
+  return(last_height)
+}
+
+#' @rdname scroll_down_and_load
+#' @param remDr Remote client driver
+#' @export
+scroll_to_top <- function(remDr = remDr) {
+  remDr$executeScript("window.scrollTo(0, 0);", args = list(1))
+}
+
+#' @rdname scroll_down_and_load
+#' @param remDr Remote client driver
+#' @param perc Incrementally scroll down the page
+#' @export
+scroll_down_page_perc <- function(remDr = remDr, perc = seq(0, 1, .005)) {
+
+  grocerycart::scroll_to_top(remDr)
+
+  last_height <-
+    remDr$executeScript("return document.body.scrollHeight") %>%
+    unlist()
+
+  purrr::map(perc, function(.x) {
+    last_height <- last_height * .x
+    remDr$executeScript(
+      script = stringr::str_glue("window.scrollTo({{
+                                 left: 0,
+                                 top: {last_height},
+                                 behavior: 'smooth'
+                                 }});"))
+
+    grocerycart::nytnyt(c(4, 6), crayon_col = crayon::yellow, "Scrolled down ",
+                        .x * 100, "%\n")
+  }
+  )
+  cat(crayon::green("Reached bottom of page \n"))
+}
+
+#' @rdname scroll_down_and_load
+#' @param remDr Remote client driver
+#' @param perc Incrementally scroll up the page
+#' @export
+scroll_up_page_perc <- function(remDr = remDr, perc = seq(0, 1, .0025)) {
+
+  grocerycart::scroll_down_and_load(remDr)
+
+  last_height <-
+    remDr$executeScript("return document.body.scrollHeight") %>%
+    unlist()
+
+  purrr::map(rev(perc), function(.x) {
+    last_height <- last_height * .x
+    remDr$executeScript(
+      script = stringr::str_glue("window.scrollTo({{
+                                 left: 0,
+                                 top: {last_height},
+                                 behavior: 'smooth'
+                                 }});"))
+
+    grocerycart::nytnyt(c(2, 3), crayon_col = crayon::yellow, "Scrolled up ",
+                        100 - (.x * 100), "%\n")
+  }
+  )
+  cat(crayon::green("Reached top of page \n"))
+}
