ba82e8dd3f29c3385a0c6037d71c1937c78a475d
moamiristat <moamiristat@protonmail.com>
2022-03-07 21:40:17 -0500

    Implement empty_to_na

---
+++ R/empty_to_na.R
@@ -0,0 +1,23 @@
+#' Convert empty and null strings to NA
+#'
+#' @param x A character string
+#'
+#' @return NA
+#' @export
+#'
+#' @examples
+#' x <- "this is text"
+#' empty_to_na(x)
+#'
+#' y <- " "
+#' empty_to_na(y)
+#'
+#' z <- NULL
+#' empty_to_na(z)
+empty_to_na <- function(x) {
+  if(x == "" || is.na(x) || is.null(x) || purrr::is_empty(x)) {
+    print(NA)
+  } else {
+    x
+  }
+}
